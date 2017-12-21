19bff361e2142739e678d766564c3489d8beeb17
xxh13 <15850781776@163.com>
2017-12-21 20:00:17 +0800

    增加对于导航栏的状态改变

--- src/component/pageTitle.js
+++ src/component/pageTitle.js
@@ -1,23 +1,63 @@
 import React from 'react'
 
-class DashboradSpan extends React.Component {
-
-    render() {
-        return(
-            <span className="fa fa-home" />
-        )
-    }
-}
-
-
 class PageTitle extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+        let currentLocation = window.location.href;
+        if (currentLocation.endsWith('/')) {
+            currentLocation = currentLocation.substr(0, currentLocation.length - 1);
+        }
+        let tempArray = currentLocation.split("/");
+        let category = tempArray.slice(-2)[0];
+        let detail = tempArray.slice(-1)[0];
+
+        let angleSpan = "<span class=\"fa fa-angle-right page-title-arrow\"/>";
+
+        let navTitle = document.getElementById("navbar-title");
+        navTitle.innerHTML = "<span class=\"fa fa-home\"/>主页";
+
+        if (detail !== "dashboard") {
+            if (category === "qrcode") {
+                navTitle.innerHTML += angleSpan;
+                navTitle.innerHTML += "<span class=\"fa fa-qrcode\"/>二维码";
+                if (detail === "view") {
+                    navTitle.innerHTML += angleSpan;
+                    navTitle.innerHTML += "二维码统计";
+                }
+            }
+            else if (category === "machine") {
+                navTitle.innerHTML += angleSpan;
+                navTitle.innerHTML += "<span class=\"fa fa-laptop\"/>机器";
+                if (detail === "view") {
+                    navTitle.innerHTML += angleSpan;
+                    navTitle.innerHTML += "机器统计";
+                }
+            } else if (category === "consumer") {
+                navTitle.innerHTML += angleSpan;
+                navTitle.innerHTML += "<span class=\"fa fa-user\"/>客户";
+                if (detail === "view") {
+                    navTitle.innerHTML += angleSpan;
+                    navTitle.innerHTML += "客户统计";
+                }
+            } else if (category === "order"){
+                navTitle.innerHTML += angleSpan;
+                navTitle.innerHTML += "<span class=\"fa fa-laptop\"/>订单";
+                if (detail === "view") {
+                    navTitle.innerHTML += angleSpan;
+                    navTitle.innerHTML += "订单统计";
+                }
+            }
+        }
+    }
 
     render() {
         return(
             <div className="pagetitle">
                 <h5 id="navbar-title" className="title">
-                    <span className="fa fa-home"/>
-                    主页
                 </h5>
 
             </div>
